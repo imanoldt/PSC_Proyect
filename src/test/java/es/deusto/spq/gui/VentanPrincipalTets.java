d9cbf040640caa066a6bc890d6949baeb2c0bff1
Jonathan Barroeta <jbarroeta@opendeusto.es>
2023-04-30 19:56:45 +0200

    Prueba

--- src/test/java/es/deusto/spq/gui/VentanPrincipalTets.java
+++ src/test/java/es/deusto/spq/gui/VentanPrincipalTets.java
@@ -2,26 +2,32 @@ package es.deusto.spq.gui;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import es.deusto.spq.client.gui.VentanaLoginN;
 import es.deusto.spq.client.gui.VentanaPrincipal;
+import es.deusto.spq.pojo.Usuario;
 
 
 public class VentanPrincipalTets {
 	@Mock
-	String usuario;
-	@Mock
-	String contraseña;
-	@Mock
-	String tipo;
+	Usuario u;
 	@Mock
 	VentanaPrincipal vp;
 	
+	@Before
+	public void setUp() {
+		u = new Usuario();
+		u.setName("name");
+		u.setPassword("pass");
+	}
 	@Test
 	public void test() {
 		try {
-			VentanaPrincipal vp = new VentanaPrincipal(usuario,contraseña,tipo);
+			VentanaPrincipal vp = new VentanaPrincipal(u.getName(),u.getPassword(),"tipo");
 		}catch(Exception e) {
 			assertTrue(true);
 		}
--- src/test/java/es/deusto/spq/gui/VentanaMenuNTest.java
+++ src/test/java/es/deusto/spq/gui/VentanaMenuNTest.java
@@ -2,24 +2,32 @@ package es.deusto.spq.gui;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import es.deusto.spq.client.gui.VentanaLoginN;
 import es.deusto.spq.client.gui.VentanaMenuN;
+import es.deusto.spq.pojo.Usuario;
 
 
 public class VentanaMenuNTest {
 	@Mock
-	String usuario;
-	@Mock
-	String contraseña;
+	Usuario u;
 	@Mock
 	VentanaMenuN vm;
 	
+	@Before
+	public void setUp() {
+		u = new Usuario();
+		u.setName("user");
+		u.setPassword("pass");
+		}
 	@Test
 	public void test() {
 		try {
-			VentanaMenuN vm = new VentanaMenuN(usuario,contraseña);
+			VentanaMenuN vm = new VentanaMenuN(u.getName(),u.getPassword());
 		}catch(Exception e) {
 			assertTrue(false);
 		}
