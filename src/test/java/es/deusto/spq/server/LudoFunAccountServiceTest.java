4f7c0008ab0b76805952830b45846e3ea38e63f1
Jonathan Barroeta <jbarroeta@opendeusto.es>
2023-05-19 13:19:49 +0200

    package server 25% de cobertura
    Signed-off-by: Jonathan Barroeta <jbarroeta@opendeusto.es>

---
+++ src/test/java/es/deusto/spq/server/LudoFunAccountServiceTest.java
@@ -0,0 +1,108 @@
+//package es.deusto.spq.server;
+//
+//import static org.junit.Assert.*;
+//
+//
+//
+//import javax.jdo.JDOHelper;
+//import javax.jdo.PersistenceManager;
+//import javax.jdo.PersistenceManagerFactory;
+//import javax.jdo.Transaction;
+//import javax.ws.rs.client.WebTarget;
+//
+//import org.junit.After;
+//
+//import org.junit.BeforeClass;
+//import org.junit.Rule;
+//import org.junit.Test;
+//
+//import com.github.noconnor.junitperf.JUnitPerfRule;
+//import com.github.noconnor.junitperf.reporting.providers.HtmlReportGenerator;
+//
+//import org.glassfish.grizzly.http.server.HttpServer;
+//
+//
+//import es.deusto.spq.server.jdo.User;
+//
+//public class LudoFunAccountServiceTest {
+//
+//	private LudoFunAccountService lf;
+//	private LudoFunAccountService lf2;
+//
+//	private static final PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+//
+//
+//	private WebTarget target;
+//	private static HttpServer server;
+//
+//	@Rule
+//	public JUnitPerfRule perfTestRule = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.html"));
+//
+//	@BeforeClass
+//	public static void prepareTests() throws Exception {
+//		// start the server
+//		server = Main.startServer();
+//
+//		PersistenceManager pm = pmf.getPersistenceManager();
+//		Transaction tx = pm.currentTransaction();
+//		try {
+//			tx.begin();
+//			pm.makePersistent(new User("john", "1234"));
+//			tx.commit();
+//		} finally {
+//			if (tx.isActive()) {
+//				tx.rollback();
+//			}
+//			pm.close();
+//		}
+//	}
+//
+////	@Before
+////	public void setUp() {
+////		// create the client
+////		Client c = ClientBuilder.newClient();
+////		target = c.target(Main.BASE_URI).path("resource");
+////	}
+////
+////	@AfterClass
+////	public static void tearDownServer() throws Exception {
+////		server.shutdown();
+////
+////		PersistenceManager pm = pmf.getPersistenceManager();
+////		Transaction tx = pm.currentTransaction();
+////		try {
+////			tx.begin();
+////			pm.newQuery(User.class).deletePersistentAll();
+////			pm.newQuery(Message.class).deletePersistentAll();
+////			tx.commit();
+////		} finally {
+////			if (tx.isActive()) {
+////				tx.rollback();
+////			}
+////			pm.close();
+////		}
+////	}
+//
+////	@Before
+////	public void setUp() throws Exception {
+////		lf2 = LudoFunAccountService.getInstance();
+////	}
+//
+//	@After
+//	public void tearDown() throws Exception {
+//	}
+//
+//	@Test
+//	public void testInst() {
+//		lf = LudoFunAccountService.getInstance();
+//		assertEquals(lf2, lf);
+//	}
+//
+//	@Test
+//	public void testInstError() {
+//		lf = null;
+//		lf = LudoFunAccountService.getInstance();
+//		assertEquals(lf2, lf);
+//	}
+//
+//}
---
+++ src/test/java/es/deusto/spq/server/LudoFunBooksServiceTest.java
@@ -0,0 +1,51 @@
+package es.deusto.spq.server;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.spq.client.ClientController;
+import es.deusto.spq.server.jdo.Libro;
+
+public class LudoFunBooksServiceTest {
+
+	private LudoFunBooksService lfb;
+	private LudoFunBooksService lfb2;
+	
+	@Before
+	public void setUp() throws Exception {
+		lfb = LudoFunBooksService.getInstance();
+		lfb2 = LudoFunBooksService.getInstance();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInst() {
+		lfb = LudoFunBooksService.getInstance();
+		assertEquals(lfb2, lfb);
+	}
+
+	@Test
+	public void testInstError() {
+		lfb = null;
+		lfb = LudoFunBooksService.getInstance();
+		assertEquals(lfb2, lfb);
+	}
+	
+	@Test
+	public void testPopu() {
+		Libro l1 = new Libro("El capital", "Obra de Karl Marx y Engels en la que bla bla bla", (float) 10.0, "alquiler");
+		ArrayList<Libro> bks = new ArrayList<Libro>();
+		lfb.populateDB();
+		bks.add(l1);
+		assertEquals(l1 ,bks.get(0));
+	}
+
+}
